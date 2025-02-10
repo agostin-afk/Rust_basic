40d09d9bc1701aa031eb8acb5e5a9d595a873f09
Agostinho Ferreira <agostinho008.af45@gmail.com>
2025-02-09 21:55:11 -0300

    __init__

---
+++ project_command_line/target/debug/deps/project_command_line-48cd7d6f4bd48ed7.d
@@ -0,0 +1,5 @@
+/home/agostoArch/Git_repo/Rust_basic/project_command_line/target/debug/deps/libproject_command_line-48cd7d6f4bd48ed7.rmeta: src/main.rs
+
+/home/agostoArch/Git_repo/Rust_basic/project_command_line/target/debug/deps/project_command_line-48cd7d6f4bd48ed7.d: src/main.rs
+
+src/main.rs:
---
+++ project_command_line/target/debug/project_command_line.d
@@ -0,0 +1,1 @@
+/home/agostoArch/Git_repo/Rust_basic/project_command_line/target/debug/project_command_line: /home/agostoArch/Git_repo/Rust_basic/project_command_line/src/main.rs
