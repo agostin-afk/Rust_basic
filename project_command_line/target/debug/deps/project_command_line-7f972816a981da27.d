9cdafc26cb9de8974bdc9a7a5e30f5834700ca39
Agostinho Ferreira <agostinho008.af45@gmail.com>
2025-02-10 18:30:28 -0300

    remove project

--- project_command_line/target/debug/deps/project_command_line-7f972816a981da27.d
+++
@@ -1,5 +0,0 @@
-/home/agostoArch/Git_repo/Rust_basic/project_command_line/target/debug/deps/libproject_command_line-7f972816a981da27.rmeta: src/main.rs
-
-/home/agostoArch/Git_repo/Rust_basic/project_command_line/target/debug/deps/project_command_line-7f972816a981da27.d: src/main.rs
-
-src/main.rs:
--- project_command_line/target/debug/deps/project_command_line-9da3f09b800201df.d
+++
@@ -1,5 +0,0 @@
-/home/agostoArch/Git_repo/Rust_basic/project_command_line/target/debug/deps/project_command_line-9da3f09b800201df: src/main.rs
-
-/home/agostoArch/Git_repo/Rust_basic/project_command_line/target/debug/deps/project_command_line-9da3f09b800201df.d: src/main.rs
-
-src/main.rs:
