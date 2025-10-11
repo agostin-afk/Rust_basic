4cf1ecad0ea17164cc9e9a397f05ee57a1be40d1
Agostinho Ferreira <agostinho008.af45@gmail.com>
2025-10-11 18:37:54 -0300

    async buttons

--- advanced/hot_dog/src/main.rs
+++ advanced/hot_dog/src/main.rs
@@ -67,21 +67,34 @@ fn Title() -> Element {
         }
     }
 }
+
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
+
 #[component]
 fn DogView() -> Element {
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
+
     rsx! {
-        div {
-            id: "dogview",
-            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+        div { id: "dogview",
+            img { src: img_src.cloned().unwrap_or_default() }
         }
-        div {
-            id: "buttons",
-            button { id: "skip", "skip" }
-            button { id: "save", "save!" }
+        div { id: "buttons",
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
+            button { onclick: move |_| img_src.restart(), id: "save", "save!" }
         }
     }
 }
-
 fn main() {
     // launch(DogApp);
     // launch(AppBasic);
